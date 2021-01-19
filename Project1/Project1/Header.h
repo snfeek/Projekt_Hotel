441d87bff660b4dd6c8bd19edd8318bc425c9265
Łukasz Belniak <luki6405@gmail.com>
2021-01-19 22:30:46 +0100

    rachunek() w headerze (switch)

--- Project1/Project1/Header.h
+++ Project1/Project1/Header.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <iostream>
 using namespace std;
 
 class Hotel {
@@ -23,11 +24,12 @@ public:
 	const int pietro;
 	int rodzaj_pokoj; //1-jednosobowy, 2-dwuuosobowy,4-czterosobowy
 	int cena_za_noc;
-	int dzien_konca_r, miesiac_konca_r, rok_konca_r, dzien_poczatku_r, miesiac_poczatku_r, rok_poczatku_r;
-	int dzien_konca_n, miesiac_konca_n, rok_konca_n, dzien_poczatku_n, miesiac_poczatku_n, rok_poczatku_n;
+	int dzien_konca_r, miesiac_konca_r, rok_konca_r, dzien_poczatku_r, miesiac_poczatku_r, rok_poczatku_r; // dzien konca n -dzien konca nowej rezerwacji itd
+	int dzien_konca_n, miesiac_konca_n, rok_konca_n, dzien_poczatku_n, miesiac_poczatku_n, rok_poczatku_n; // dzien poczatku r - dzien poczatku starej rezerwacji
 	bool czy_zarezerowany;
 	void rezerwowanie();
 	void rezerwacja();
+	void rachunek();
 };
 class user {
 public:
@@ -45,7 +47,7 @@ void Pokoj::rezerwacja()
 
 	rok_konca_r = rok_konca_n;
 	rok_poczatku_r = rok_poczatku_n;
-	dzien_poczatku_r = dzien_poczatku_n;
+	dzien_poczatku_r = dzien_poczatku_n; //tylko jak te zmienne maja byc wprowadzane poza metoda klasy to trzeba je przeniesc do maina chyba
 	dzien_konca_r = dzien_konca_n;
 	miesiac_konca_r = miesiac_konca_n;
 	miesiac_poczatku_r = miesiac_poczatku_n;
@@ -153,6 +155,60 @@ void Pokoj::rezerwowanie() // tutaj przed wywolaniem tej funkcji dobrze by bylo 
 	}
 
 	
+}
+
+void Pokoj::rachunek()
+{
+	int rachunek_do_zaplacenia;
+	int ilosc_dni;
+	if (dzien_poczatku_n >= dzien_konca_n)
+	{
+	
+
+		switch (miesiac_konca_n - miesiac_poczatku_n)
+		{
+		case 0:
+			ilosc_dni = dzien_konca_n - dzien_poczatku_n;
+			break;
+		case 1:
+			ilosc_dni = 30 - dzien_poczatku_n + dzien_konca_n; // na zmiane dodaje 30/31 zeby bylo jakby miesiac sie zmienial a dni sie zgadzaly
+			break;
+		case 2:
+			ilosc_dni = 61 - dzien_poczatku_n + dzien_konca_n;
+			break;
+		case 3:
+			ilosc_dni = 91 - dzien_poczatku_n + dzien_konca_n;
+			break;
+		case 4:
+			ilosc_dni = 122 - dzien_poczatku_n + dzien_konca_n;
+			break;
+		case 5:
+			ilosc_dni = 152 - dzien_poczatku_n + dzien_konca_n;
+			break;
+		case 6:
+			ilosc_dni = 183 - dzien_poczatku_n + dzien_konca_n;
+			break;
+		case 7:
+			ilosc_dni = 213 - dzien_poczatku_n + dzien_konca_n;
+			break;
+		case 8:
+			ilosc_dni = 244 - dzien_poczatku_n + dzien_konca_n;
+			break;
+		case 9:
+			ilosc_dni = 247 - dzien_poczatku_n + dzien_konca_n;
+			break;
+		case 10:
+			ilosc_dni = 278 - dzien_poczatku_n + dzien_konca_n;
+			break;
+		case 11:
+			ilosc_dni = 308 - dzien_poczatku_n + dzien_konca_n; // jak ja to wykombinowalem to do teraz nie wiem xDDD
+			break;
+		}
+	}
+	//ilosc_dni = dzien_konca_n - dzien_poczatku_n; //jeszcze musze zrobic ify o to ze jak przechodzi z miesiaca na miesiac to ilosc dni + 30/31 zaleznie od miesiaca UPDATE jest na górze xD
+	
+	rachunek_do_zaplacenia = ilosc_dni * cena_za_noc;
+	cout << "Rachunek wynosi: " << rachunek_do_zaplacenia << "zl. " << endl;
 }
 
 void user::rejestracja()
