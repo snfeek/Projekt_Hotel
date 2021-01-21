2a491e29d8e74be62c8c9abe1108b3f752646544
Łukasz Belniak <luki6405@gmail.com>
2021-01-21 02:05:30 +0100

    koncowe zmiany + wizualne

--- Project1/Project1/Main.cpp
+++ Project1/Project1/Main.cpp
@@ -2,11 +2,12 @@
 #include <string>
 #include <fstream>
 #include "Header.h"
+#include <Windows.h>
 using namespace std;
 
 
 
-class User {
+class User {  //klasa uzytkownika
 public:
 	string login;
 	string haslo;
@@ -15,7 +16,7 @@ public:
 public:
 	User() {}
 
-	User(string login1, string haslo1) {
+	User(string login1, string haslo1) {		//konstruktor uzytkownika
 		login = login1;
 		haslo = haslo1;
 	}
@@ -23,11 +24,12 @@ public:
 
 bool rejestracja()
 {
-	cout << "Podaj login: ";
+	cout << "\n\t\t\t\t*************";
+	cout <<"\n\t\t\t\t Podaj login: ";
 
 	string login;
 	cin >> login;
-	cout << "\nPodaj haslo: ";
+	cout << "\n\t\t\t\t Podaj haslo: ";
 	string haslo;
 	cin >> haslo;
 
@@ -55,7 +57,7 @@ bool rejestracja()
 		}
 
 		else
-			cout << "Jest juz taki uzytykownik! Login zajety" << endl;
+			cout << "\n\t\t\t\tJest juz taki uzytykownik! Login zajety" << endl;
 		return 0;
 	}
 
@@ -68,14 +70,15 @@ void menu_admin()				//po zalogowaniu
 
 }
 
-bool zalogowany = false;
+bool zalogowany = false;  //zmienna do nawigowania w menu, nie pozwala zarezerwowac pokoju jezeli uzytkownik nie jest zalogowany
 
 bool logowanie()
 {
 	string login, haslo;
-	cout << "Podaj login: ";
+	system("cls");
+	cout << "\n\t\t\t\tPodaj login: ";
 	cin >> login;
-	cout << "\nPodaj haslo: ";
+	cout << "\n\t\t\t\tPodaj haslo: ";
 	cin >> haslo;
 
 	if (login.compare("admin") == 0 && haslo.compare("admin") == 0)
@@ -105,7 +108,7 @@ bool logowanie()
 			zalogowany = true;
 		}
 		else {
-			cout << "nie  zalogowany" << endl;
+			cout << "\n\t\t\t\tnie  zalogowany" << endl;
 			return 0;
 		}
 
@@ -115,15 +118,9 @@ bool logowanie()
 	return 1;
 }
 
-void zarzadzanie_pokojem()
-{
 
-}
 
-void zarezerwuj_pokoj()
-{
-	
-}
+
 
 void menu_user()
 {
@@ -131,15 +128,17 @@ void menu_user()
 	{
 		system("cls");
 		int wybor;
-		cout << "** Menu **"<<endl;
-		cout << "(1) Zarezerwuj pokoj" << endl;
-		cout << "(2) Zarezerwowane pokoje" << endl;
+		cout << "\n\t\t\t\t*************";			//gwiazdki przy menu maja charakter czysto wizualny
+		cout << "\n\t\t\t\t  ** Menu **";
+		cout << "\n\t\t\t\t*************";
+		cout << "\n\t\t\t\t(1) Zarezerwuj pokoj" << endl;
+		
 		
 
 		cin >> wybor;
 		cin.clear();
 		cin.ignore();
-		system("cls"); //czyszczenie ekranu
+		system("cls");	//czyszczenie ekranu
 
 		switch (wybor)
 		{
@@ -150,7 +149,7 @@ void menu_user()
 		}
 		case 2:
 		{
-			//zarzadzanie_pokojem();
+			
 			break;
 		}
 		case 3:
@@ -177,16 +176,18 @@ void menu() {
 	for (;;)
 	{
 		int wybor;
-		cout << "Witaj w menu!" << endl;
-		cout << "Co chcesz zrobic?" << endl;
-		cout << "(1) Zaloguj sie" << endl;
-		cout << "(2) Zarejestruj sie" << endl;
-		cout << "(3) Zarezerwuj pokoj" << endl;
+		cout << "\n\t\t\t\t*******************";
+		cout << "\n\t\t\t\t** Witaj w menu! **" ;			//gwiazdki przy menu maja charakter czysto wizualny
+		cout << "\n\t\t\t\t*******************";
+		cout << "\n\t\t\t\tCo chcesz zrobic?" << endl;
+		cout << "\n\t\t\t\t(1) Zaloguj sie" << endl;
+		cout << "\n\t\t\t\t(2) Zarejestruj sie" << endl;
+		cout << "\n\t\t\t\t(3) Zarezerwuj pokoj" << endl;
 
 		cin >> wybor;
 		cin.clear();
 		cin.ignore();
-		system("cls"); //czyszczenie ekranu
+		system("cls");		 //czyszczenie ekranu
 
 		switch (wybor)
 		{
@@ -202,7 +203,7 @@ void menu() {
 		}
 		case 2:
 		{
-			rejestracja();
+			rejestracja();					//wywolanie rejestracji
 			
 			
 			break;
@@ -218,27 +219,28 @@ void menu() {
 		}
 		if (wybor == 3)
 		{
-			if (zalogowany)
+			if (zalogowany)			//ograniczenie do rezerwacji
 				break;
 			else
-				cout << "Musisz sie zalogowac" << endl;
+				cout << "\n\t\t\t\tMusisz sie zalogowac" << endl;
 		}
 	}
 }
 
+
 int main()
 {
 	
-	Pokoj p[10];  //tworzenie obiektów pokoi 
+	Pokoj p[10];						 //tworzenie obiektów pokoi 
 	for (int i = 0; i < 10; i++)
 	{
-		p[i].numer_pokoju = i + 1; //nadawanie pokojom numerów
+		p[i].numer_pokoju = i + 1;		//nadawanie pokojom numerów
 	}
 	for (int i = 0; i < 3; i++)
 	{
-		p[i].standard = 1;			//standard
-		p[i].pietro = 0;			//3 pierwsze pokoje na parterze
-		p[i].liczba_lozek = 2;		// 1 standard pokoju ma po dwa ³ó¿ka i oba pojedynce
+		p[i].standard = 1;				//standard
+		p[i].pietro = 0;				//3 pierwsze pokoje na parterze
+		p[i].liczba_lozek = 2;			// 1 standard pokoju ma po dwa ³ó¿ka i oba pojedynce
 		p[i].rodzaj_lozek = "Pojedyncze";
 		p[i].cena_za_noc = 150;
 
@@ -274,14 +276,97 @@ int main()
 
 	for (int i = 0; i < 10; i++)
 	{
-		p[i].pokaz_dane();
+		p[i].pokaz_dane();   //wyswietlenie pokoi
 	}
-	cout << endl << " Wybierz pokoj do zarezerwowania: ";
+	cout << endl << "\n\t\t\t\t Wybierz pokoj do zarezerwowania: "; //wyjscie z menu, rezerwowanie
 	int choice;
 	cin >> choice;
-	p[choice-1].rezerwowanie();
-	p[choice-1].rachunek();
+	cin.clear();
+	cin.ignore();
+	
+	p[choice-1].rezerwowanie(); // choice -1 poniewa¿ u¿ytkownik wpisuje od 1 do 10, a tablica ma wartosci od 0 do 9
+	
+	
+	
 
+	system("cls");
+	cout << "\n\t\t\t\t(1) Informacje o rezerwacji" << endl;			//dalsze operacje na rezerwacji
+	cout << "\n\t\t\t\t(2) Edycja rezerwacji" << endl;
+	int wybor2;
+	cin >> wybor2;
+	cin.clear();
+	cin.ignore();
+
+	switch (wybor2)
+	{
+	case 1:
+	{
+		system("cls");
+		cout << "\n\t\t\t\t*************";				//gwiazdki maja charakter czysto wizualny
+		cout  << "\n\t\t\t\tInformacje o rezerwacji" ;
+		cout << "\n\t\t\t\t*************";
+		cout << "\n\t\t/t/tZarezerwowany pokoj: pokoj numer " << p[choice - 1].numer_pokoju << endl;			//wyswietla informacje o rezerwacji takie jak numer pokoju i rachunek za rezerwacje
+		p[choice - 1].rachunek();																	
+		Sleep(10000);								//sleep aby system przez 10 sekund pokaza³ dan¹ informacje a potem wróci³ do menu
+		system("cls");
+		break;
+	}
+	case 2:
+	{
+		cout << "\n\t\t\t\t*************";					//gwiazdki maja charakter czysto wizualny
+		cout << "\n\t\t\t\t(1) zmiana pokoju" ;
+		cout << "\n\t\t\t\t(2) zmiana terminu rezerwacji";
+		cout << "\n\t\t\t\t*************";
+		int wybor3;
+		cin >> wybor3;
+		cin.clear();
+		cin.ignore();
+		switch (wybor3)
+		{
+		case 1:
+			{
+
+			system("cls");
+			cout << "\n\t\t\t\t*************";						//gwiazdki maja charakter czysto wizualny
+			cout << "\n\t\t\t\tzmiana pokoju" << endl;
+			cout << "\n\t\t\t\t*************";
+			for (int i = 0; i < 10; i++)
+				{
+					p[i].pokaz_dane();   //wyswietlenie pokoi
+					
+				}
+			cout << endl << endl << "\n\t\t\t\tWybierz nowy pokoj" << endl;				//wybieranie nowego pokoju
+			int wybor4;
+			cin >> wybor4;
+			choice = wybor4;
+			cin.clear();
+			cin.ignore();
+			p[choice - 1].rezerwowanie();
+			cout << "\n\t\t\t\tUdalo sie zmienic pokoj" << endl;
+			Sleep(10000);
+			system("cls");
+			break;
+			}
+		case 2:
+			{
+			system("cls");
+			cout << "\n\t\t\t\t*************";					//gwiazdki maja charakter czysto wizualny
+			cout << "\n\t\t\t\tzmiana terminu rezerwacji"  ;
+			cout << "\n\t\t\t\t*************";
+			p[choice - 1].rezerwowanie();
+			cout << "\n\t\t\t\tudalo sie zmienic termin rezerwacji" << endl;				//wybieranie nowego terminu dla rezerwacji
+			Sleep(10000);	
+			system("cls");
+			break;
+			}
+		}
+		Sleep(10000);
+		system("cls");
+		break;
+	}
+	}
+		
+	menu();
 
 	system("pause");
 	return 0;
